b56ca196cafcaa41568d3f55c32f65e0d29ec01a
Joaquin Allende <joacoallendeids@gmail.com>
2019-05-01 17:11:45 -0300

    Agrega la informacion al archivo database.js que permite realizar la conexion a la base de datos

---
+++ Server/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'allende247855',
+    database: 'TorneoCopaCiudadDeAyacucho'
+});
+
+mysqlConnection.connect(function(err){
+    if (err){
+        console.log(err);
+        return;
+    } else {
+        console.log('db is connected');
+    }
+}); 
+
+module.exports = mysqlConnection;
